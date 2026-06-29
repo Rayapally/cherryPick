7adcee67f6f65dd489be93f711d69aeb384e3af8
Rayapally <rayapallyvikas1@gmail.com>
2026-06-29 16:36:01 +0530

    added popup code in this script.js

--- script.js
+++ script.js
@@ -2,3 +2,17 @@ function changeText() {
     document.getElementById("message").innerHTML =
     "🎉 Congratulations! JavaScript is Working.";
 }
+function changeText() {
+
+    let name = prompt("Enter your name:");
+
+    if(name == "" || name == null){
+        document.getElementById("message").innerHTML =
+        "Please enter your name!";
+    }
+    else{
+        document.getElementById("message").innerHTML =
+        "Welcome, " + name + " 😊";
+    }
+
+}
